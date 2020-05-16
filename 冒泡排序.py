08e7df9248692064a78769f580d8011e560ac3ed
JulyLi2019 <49575879+JulyLi2019@users.noreply.github.com>
2020-05-17 02:42:34 +0800

    Add files via upload

---
+++ 冒泡排序.py
@@ -0,0 +1,8 @@
+def bubbleSort(alist):
+    for passnum in range(len(alist) - 1, 0, -1):
+        for i in range(passnum):
+            if alist[i] > alist[i + 1]:
+                alist[i], alist[i + 1] = alist[i + 1], alist[i]
+    return alist
+
+print(bubbleSort([5,3,2,4,1]))
